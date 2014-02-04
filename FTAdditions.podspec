a7f752c1af3769b2c243ae0b4c5f141dc199a52e
Juan-Carlos Foust <jc@fototropik.com>
2014-02-04 01:40:21 -0800

    update spec to use master branch, fix more typos

--- FTAdditions.podspec
+++ FTAdditions.podspec
@@ -5,7 +5,7 @@ Pod::Spec.new do |s|
   s.homepage     = "https://github.com/kharmabum/FTAdditions"
   s.license      = "MIT"
   s.author       = { "Juan-Carlos Foust" => "jc@fototropik.com" }
-  s.source       = { :git => "https://github.com/kharmabum/FTAdditions.git", :tag => s.version.to_s }
+  s.source       = { :git => "https://github.com/kharmabum/FTAdditions.git", :branch => "master" }
   s.platform     = :ios, "7.0"
   s.source_files = "*.{h,m}", "Categories/*.{h,m}", "Vendor/*.{h,m}", "Util/*.{h,m}"
   s.frameworks   = "UIKit", "CoreGraphics", "QuartzCORE"
--- Util/FTDebug.h
+++ Util/FTDebug.h
@@ -16,13 +16,13 @@
 #define DEBUG_ON  // Toggle to DEBUG_OFF to hide all debug code
 
 #ifdef DEBUG_ON
-#define debug(format, …) CFShow((__bridge void *)[NSString stringWithFormat:format, ## __VA_ARGS__]);
+#define debug(format, ...) CFShow((__bridge void *)[NSString stringWithFormat:format, ## __VA_ARGS__]);
 #else
 #define debug(format, ...)
 #endif
 
 #define debugRect(rect) debug(@"%s x:%.4f, y:%.4f, w:%.4f, h%.4f", #rect,rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
 #define debugSize(size) debug(@"%s w:%.4f, h:%.4f", #size, size.width, size.height)
-#define debugPoint(point) debug(@"%s x:%.4f, y:%.4f", #pt, pt.x, pt.y)
+#define debugPoint(point) debug(@"%s x:%.4f, y:%.4f", #point, point.x, point.y)
 
 #endif
